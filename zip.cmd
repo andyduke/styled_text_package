5324b1b99b0543f48af1dea31accc30ab91029eb
Andy Chentsov <chentsov@gmail.com>
2019-05-17 17:48:52 +0300

    Example added

--- zip.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-for %%a in (.) do set currentfolder=%%~na
-rem echo %currentfolder%
-
-pkzipc -max -add -dir -excl=zip.cmd -excl=build -excl=build/* -excl=android/.gradle -excl=android/.gradle/* %currentfolder%-%date:~6%-%date:~3,2%-%date:~0,2%.zip *.*
-
-endlocal
