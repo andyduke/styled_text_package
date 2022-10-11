b8cd67f7917d9975166a519c108e9dd94f59ef80
Andy Chentsov <chentsov@gmail.com>
2022-10-11 19:55:53 +0300

    Migrate to Flutter 3.3

---
+++ styled_text/example/android/app/src/main/kotlin/com/example/styled_text_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.styled_text_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- styled_text/example/ios/Flutter/flutter_export_environment.sh
+++ styled_text/example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\Flutter2Live\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Flutter2Live\_projects\styled_text\styled_text\example"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter2Live\_projects\styled_text\_temp\3.3.4"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.0"
-export "FLUTTER_BUILD_NUMBER=2.0.0"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
