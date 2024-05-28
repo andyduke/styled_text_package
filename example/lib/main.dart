7db6f90aa4ac5905df798e3c694b9356856605b3
Andy Chentsov <chentsov@gmail.com>
2024-05-28 16:32:22 +0300

    Upgrading example projects to Flutter 3.2.0 and Dart 3.16.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,17 +9,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // showPerformanceOverlay: true,
       title: 'StyledText Demo',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const DemoPage(),
+      home: const StartPage(),
     );
   }
 }
 
-class DemoPage extends StatelessWidget {
-  const DemoPage({super.key});
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  bool async = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StyledText Demo'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Switch.adaptive(
+                    value: async,
+                    onChanged: (value) => setState(() {
+                      async = value;
+                    }),
+                  ),
+                  const SizedBox(width: 8.0),
+                  const Text('Async?'),
+                ],
+              ),
+
+              //
+              const Divider(),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SimpleDemoPage(async: async)),
+                  );
+                },
+                child: const Text('Simple Demo'),
+              ),
+
+              //
+              const SizedBox(height: 24),
+
+              //
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LargeTextDemo(async: async)),
+                  );
+                },
+                child: const Text('Large Text Demo'),
+              ),
+
+              //
+              const SizedBox(height: 24),
+
+              //
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ListDemo(async: async)),
+                  );
+                },
+                child: const Text('List Demo'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SimpleDemoPage extends StatelessWidget {
+  final bool async;
+
+  const SimpleDemoPage({
+    super.key,
+    this.async = false,
+  });
 
   void _alert(BuildContext context) {
     showDialog<void>(
@@ -67,7 +157,7 @@ class DemoPage extends StatelessWidget {
     return SelectionArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('StyledText Demo'),
+          title: const Text('Simple Demo'),
         ),
         body: Center(
           child: Column(
@@ -77,6 +167,7 @@ class DemoPage extends StatelessWidget {
             children: <Widget>[
               // Simple formatted text
               StyledText(
+                async: async,
                 text: 'Test: <b>bold</b> text.',
                 tags: {
                   'b': StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold)),
@@ -86,6 +177,7 @@ class DemoPage extends StatelessWidget {
               // Text with quotes
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: 'Quoted Test: <b>&quot;bold&quot;</b> text.',
                 tags: {
                   'b': StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold)),
@@ -95,6 +187,7 @@ class DemoPage extends StatelessWidget {
               // Multiline text without breaks
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: """Multiline text 
 (wo breaks)""",
                 tags: {
@@ -105,6 +198,7 @@ class DemoPage extends StatelessWidget {
               // Multiline text with breaks
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: """Multiline text
 (with breaks)""",
                 newLineAsBreaks: true,
@@ -116,6 +210,7 @@ class DemoPage extends StatelessWidget {
               // Custom tags styles
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: 'Test: <bold>bold</bold> and <red>red color</red> text.',
                 tags: {
                   'bold': StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold)),
@@ -126,6 +221,7 @@ class DemoPage extends StatelessWidget {
               // Icon
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: 'Text with alarm <alarm/> icon.',
                 tags: {
                   'alarm': StyledTextIconTag(Icons.alarm),
@@ -135,6 +231,7 @@ class DemoPage extends StatelessWidget {
               // Action
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: 'Text with <action>action</action> inside.',
                 tags: {
                   'action': StyledTextActionTag(
@@ -147,6 +244,7 @@ class DemoPage extends StatelessWidget {
               // Link
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: 'Text with <link href="https://flutter.dev">link</link> inside.',
                 tags: {
                   'link': StyledTextActionTag(
@@ -159,6 +257,7 @@ class DemoPage extends StatelessWidget {
               // SelectableText with Link
               const SizedBox(height: 20),
               StyledText.selectable(
+                async: async,
                 text: 'Text with <link href="https://flutter.dev">link</link> inside.',
                 tags: {
                   'link': StyledTextActionTag(
@@ -171,6 +270,7 @@ class DemoPage extends StatelessWidget {
               // Text with superscript
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: "Famous equation: E=mc<sup>2</sup>",
                 tags: {
                   'sup': StyledTextWidgetBuilderTag(
@@ -190,6 +290,7 @@ class DemoPage extends StatelessWidget {
               // Text with subscript
               const SizedBox(height: 20),
               StyledText(
+                async: async,
                 text: "The element of life: H<sub>2</sub>0",
                 tags: {
                   'sub': StyledTextWidgetBuilderTag(
@@ -207,6 +308,98 @@ class DemoPage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class LargeTextDemo extends StatefulWidget {
+  final bool async;
+
+  const LargeTextDemo({
+    super.key,
+    this.async = false,
+  });
+
+  @override
+  State<LargeTextDemo> createState() => _LargeTextDemoState();
+}
+
+class _LargeTextDemoState extends State<LargeTextDemo> {
+  Key _textKey = UniqueKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Large Text Demo'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _textKey = UniqueKey();
+              });
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: StyledText(
+            key: _textKey,
+            async: widget.async,
+            text: '''
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio, vel <b>pretium <i>ligula</i> purus</b> vel ligula. In posuere justo eget libero. Cras consequat quam sit amet metus. Sed vitae nulla. Cras imperdiet sapien vitae ipsum. Curabitur tristique. Aliquam non tellus eget sem commodo tincidunt. Phasellus cursus nunc. Integer vel mi. Aenean rutrum libero sit amet enim. Nunc elementum, erat eu volutpat ultricies, eros justo scelerisque leo, quis sollicitudin purus ipsum at purus. Aenean ut nulla.
+''',
+            tags: {
+              'b': StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold)),
+              'i': StyledTextTag(style: const TextStyle(fontStyle: FontStyle.italic)),
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ListDemo extends StatelessWidget {
+  final bool async;
+
+  const ListDemo({
+    super.key,
+    this.async = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List Demo'),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: 100,
+        itemBuilder: (context, index) => StyledText(
+          async: async,
+          text: '''
+Lorem ipsum dolor sit <b>amet</b>, consectetuer adipiscing elit. Sed <i>dapibus, ante ultricies adipiscing pulvinar</i>, enim tellus volutpat odio.
+        ''',
+          tags: {
+            'b': StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold)),
+            'i': StyledTextTag(style: const TextStyle(fontStyle: FontStyle.italic)),
+          },
         ),
       ),
     );
